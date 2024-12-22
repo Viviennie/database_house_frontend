c7606f0cdf5be4e5d9734d1cb12c7c9a770b4ede
VellazZ <2254282@tongji.edu.cn>
2024-12-22 22:57:30 +0800

    删掉分布式注释掉的代码

--- src/api/distributedService.js
+++ src/api/distributedService.js
@@ -8,23 +8,6 @@ const distributedClient = axios.create({
 });
 
 export const fetchDistributedData = (queryForm) => {
-  //console.log('进入分布式查询参数:', JSON.stringify(queryForm, null, 2));
-  //const formatDate = (date) => {
-    //try {
-      //if (!date) return '';
-      //console.log('处理日期:', date, typeof date);  // 调试日期对象
-      //const year = date.getFullYear();
-      //const month = String(date.getMonth() + 1).padStart(2, '0');
-      //const day = String(date.getDate()).padStart(2, '0');
-      //return `${year}-${month}-${day}`;
-    //} catch (error) {
-      //console.error('日期格式化错误:', error);
-      //return '';  // 返回空字符串而不是抛出错误
-    //}
-  //};
-   // 检查 releaseDate 是否存在且是数组
-  //console.log('releaseDate:', queryForm.releaseDate);
-  // 转换日期格式并确保数据是普通对象
   const formattedQuery = {
     name: queryForm.name,
     startTime: queryForm.releaseDate[0],
@@ -38,7 +21,7 @@ export const fetchDistributedData = (queryForm) => {
     highscore: queryForm.score[1],
     limit: queryForm.limit || 100, // 默认限制为 100
   };  
-  console.log('发送的分布式参数:', JSON.stringify(formattedQuery, null, 2));
+  //console.log('发送的分布式参数:', JSON.stringify(formattedQuery, null, 2));
   return distributedClient.get('/search', {
     params: formattedQuery
   });
@@ -67,20 +50,8 @@ export const fetchMostPopularActorsFromDistributed = (queryForm) => {
   });
 }; 
 
-export const fetchDirectorActorCooperation = (queryForm) => {
-  const formattedQuery = {
-    actor_name: queryForm.name,
-    limit: queryForm.limit  // 限制返回结果数量
-  };
-  
-  return distributedClient.get('/actor_director', {
-    params: formattedQuery
-  });
-}; 
-
 export const fetchCooperatingDirectorActorFromDistributed = (queryForm) => {
   const formattedQuery = {
-    //director_name: queryForm.name,
     limit: queryForm.limit  // 限制返回结果数量
   };
   
@@ -93,7 +64,6 @@ export const fetchCooperatingActorsFromDistributed = (queryForm) => {
   //console.log('进入分布式查询参数:', JSON.stringify(queryForm, null, 2));
 
   const formattedQuery = {
-    //actor_name: queryForm.name,
     limit: queryForm.limit  // 限制返回结果数量
   };
   
